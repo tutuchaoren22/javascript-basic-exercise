754c9f64b82cc29a35278c62bd07bdbb6c93ffc5
duyuqian <809009017@qq.com>
2020-07-23 17:17:35 +0800

    [duyuqian] feat-3: create an array which contains a range numbers

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -1,22 +1,32 @@
 export function range(start, end) {
-  // This function creates an array which contains numbers within a range specified by `start`
-  // and `end`.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in range_and_array_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function creates an array which contains numbers within a range specified by `start`
+    // and `end`.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in range_and_array_spec.js.
+    // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+    let rangeArray = [];
+    if (start <= end) {
+        for (let i = start; i < end; i++) {
+            rangeArray.push(i);
+        }
+    } else {
+        for (let i = start; i > end; i--) {
+            rangeArray.push(i);
+        }
+    }
+    return rangeArray;
 }
 
 export function sum(...numbers) {
-  // This function sums all the numbers in the array and returns the final result.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in range_and_array_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function sums all the numbers in the array and returns the final result.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in range_and_array_spec.js.
+    // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+    throw new Error('Please delete this line and implement the function');
 }
