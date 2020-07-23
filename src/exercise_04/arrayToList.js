71467e78f22a433f8da341e365f66b38706ee151
duyuqian <809009017@qq.com>
2020-07-23 18:37:50 +0800

    [duyuqian] feat-4: create a linked list from an array

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,10 +1,19 @@
 export default function arrayToList(array) {
-  // This function creates a linked list from an array.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in array_to_list_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function creates a linked list from an array.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in array_to_list_spec.js.
+    // * Please do NOT modify the signature of the function.
+    if (!array) {
+        throw new Error('Creating list from undefined array');
+    } else if (array.length == 0) {
+        throw new Error('Creating list from empty array');
+    } else {
+        return genList(array);
+    }
 
-  throw new Error('Please delete this line and implement the function');
+    function genList(array) {
+        return (array.length > 1) ? { value: array[0], next: genList(array.slice(1)) } : { value: array[0], next: null };
+    }
 }
