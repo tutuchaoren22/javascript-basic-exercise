4d4d66ddd9ecd2991be1834d2915d19711c4857e
duyuqian <809009017@qq.com>
2020-07-23 17:09:17 +0800

    [duyuqian] feat-2: count the character number in a string

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -1,10 +1,14 @@
 export default function countCharacter(string, prediction) {
-  // This function will count the character number in a string which satisfies specific prediction.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in character_counter_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function will count the character number in a string which satisfies specific prediction.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in character_counter_spec.js.
+    // * Please do NOT modify the signature of the function.
+    if (string) {
+        return (prediction) ? string.split('').filter(prediction).length : string.length;
+    } else {
+        return 0;
+    }
 
-  throw new Error('Please delete this line and implement the function');
 }
