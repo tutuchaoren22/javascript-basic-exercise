f70ed48b77df41ca957382291ba03198fb7c9da2
duyuqian <809009017@qq.com>
2020-07-23 19:51:58 +0800

    [duyuqian] feat-5: flatten a nest array

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,10 +1,22 @@
 export default function flattenArray(array) {
-  // This function flattens a nested array into a sequence.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in flatten_array_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function flattens a nested array into a sequence.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in flatten_array_spec.js.
+    // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+    if (Array.isArray(array)) {
+        let flattenedArray = [];
+        for (let i in array) {
+            if (Array.isArray(array[i])) {
+                flattenedArray.push(...array[i]);
+            } else {
+                flattenedArray.push(array[i]);
+            }
+        }
+        return flattenedArray;
+    } else {
+        throw new Error('Flatten undefined or null array');
+    }
 }
