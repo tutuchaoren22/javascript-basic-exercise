c0b9f3ea6396fd3ff9477540bf4e2cc5d9f6c0cc
duyuqian <809009017@qq.com>
2020-07-27 16:11:28 +0800

    [duyuqian] feat-7: return a promise when all promises are completed

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,13 +1,21 @@
 export default function waitForAll(...promises) {
-  // This function returns a promise which will be triggered when all the `promises` are completed.
-  //
-  // If all the `promises` are resolved, then the returned promise will be resolved. Otherwise,
-  // if one of the `promises` is rejected, then the returned promise will be rejected.
-  //
-  // Your target:
-  //
-  // * Please implement this function and pass all the tests in wait_for_all_spec.js.
-  // * Please do NOT modify the signature of the function.
+    // This function returns a promise which will be triggered when all the `promises` are completed.
+    //
+    // If all the `promises` are resolved, then the returned promise will be resolved. Otherwise,
+    // if one of the `promises` is rejected, then the returned promise will be rejected.
+    //
+    // Your target:
+    //
+    // * Please implement this function and pass all the tests in wait_for_all_spec.js.
+    // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+    for (let i of promises) {
+        if (!(i instanceof Promise)) {
+            throw new Error('Not all elements are promises.');
+        }
+    }
+
+    return Promise.allSettled(promises)
+        .then(allPromises => allPromises.filter(p => p.status === 'rejected'))
+        .then(resPromises => (resPromises.length > 0 ? Promise.reject() : Promise.resolve()));
 }
